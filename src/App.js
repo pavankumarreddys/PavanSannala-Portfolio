04c62a6bc9ea8f4e8c1a98839b4f6b5aa959fba5
santosh kumar <santosh>
2022-06-22 18:34:15 +0530

    title is added

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import Skills from "./components/Skills";
 import SocialLinks from "./components/SocialLinks";
-
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div >
+
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>SantoshPortfolio</title>
+        <meta name="description" content="portfolio of Santosh" />
+      </Helmet>
+
       <NavBar />
       <Home />
       <About />
